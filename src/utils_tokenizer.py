4792b5b30e8a7cd826763930e0076b650c9e33cd
nabila <n.bouras.ing@gmail.com>
2024-09-06 20:57:50 +0100

    Update utils_tokenizer.py

--- src/utils_tokenizer.py
+++ src/utils_tokenizer.py
@@ -1,10 +1,13 @@
 # Utility functions for tokenization 
-def apply_tokenizer(tokenizer, text, sum, max_len):
+def apply_tokenizer(tokenizer, text, sum, max_len): 
+  
 	# TODO: Complete this function 
 	# Concatenate text and summary and in-between add a special token 
 	# <BOS> is beginning of sentence 
 	# <EOS> is end of sentence
-	return tokenizer('<BOS>'  '<EOS>', truncation=True, max_length=max_len, padding="max_length")
+# Concatenate text and summary with special tokens <BOS> and <EOS>
+    combined_text = '<BOS> ' + text + ' <CLS> ' + sum + ' <EOS>'
+    return tokenizer(combined_text, truncation=True, max_length=max_len, padding="max_length")
 
 def tokenize_text(tokenizer, df, max_len):
     df['encodings'] = df.apply(lambda x : apply_tokenizer(tokenizer, x['text'], x['summary'], max_len), axis=1)
